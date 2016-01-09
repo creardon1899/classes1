3ddd728620a40dde207832ee3dd39a98a8355a13
Apple <Apple@apples-macbook-air.local>
2016-01-09 13:11:21 -0600

    Initial Commit

---
+++ Classes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Classes
+//
+//  Created by Apple on 1/9/16.
+//  Copyright © 2016 Connor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
